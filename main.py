4b3a1ae86360915d49f31a66f48671a3aac9b0eb
Aron <aronbaur@gmx.ch>
2023-03-24 17:14:21 +0100

    added a small demo

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
-print("test")
+import cv2
+import webbrowser
+
+rtsp_address = "rtsp://username:password@IP_address:port/stream"
+
+cap = cv2.VideoCapture(0)
+
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+motion_detected = False
+motion_counter = 0
+motion_threshold = 10000
+last_motion_time = 0
+
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Camera Feed', frame)
+
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+
+    if motion_counter == 0:
+        first_frame = gray_frame
+        motion_counter += 1
+        continue
+
+    frame_delta = cv2.absdiff(first_frame, gray_frame)
+    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+
+
+    contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < motion_threshold:
+            continue
+        motion_detected = True
+
+    if motion_counter == 30:
+        first_frame = gray_frame
+        motion_counter = 0
+
+
+    if motion_detected:
+        webbrowser.open('http://example.com')
+        motion_detected = False
+
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+    motion_counter += 1
+
+
+cap.release()
+cv2.destroyAllWindows()
