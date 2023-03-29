f9e9cdad3c4751cae3818375dcff32ca0f7b6059
AronBA <72823328+AronBA@users.noreply.github.com>
2023-03-29 23:59:25 +0200

    add settings gui and settings config

--- main.py
+++ main.py
@@ -2,33 +2,32 @@ import cv2
 import webbrowser
 import time
 import os
+from configparser import ConfigParser
 
-rtsp_address = "rtsp://username:password@IP_address:port/stream"
+config = ConfigParser()
+
+config.read("settings.ini")
+
+print(config.get("DEFAULT","program"))
 
 cap = cv2.VideoCapture(0)
-
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-filepath = rf"C:\Users\{os.getlogin()}\AppData\Local\Microsoft\WindowsApps\wt.exe"
-
+filepath = ""
 motion_detected = False
 motion_delay = 10
-prev_motion_time = 0
 motion_counter = 0
 motion_threshold = 10000
 last_motion_time = 0
-
 
 while True:
     ret, frame = cap.read()
     cv2.imshow('Camera Feed', frame)
 
-
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-
 
     if motion_counter == 0:
         first_frame = gray_frame
@@ -38,7 +37,6 @@ while True:
     frame_delta = cv2.absdiff(first_frame, gray_frame)
     thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
-
 
     contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
@@ -50,9 +48,9 @@ while True:
         first_frame = gray_frame
         motion_counter = 0
 
-
     if motion_detected and time.time() > prev_motion_time + motion_delay:
-        webbrowser.open('http://example.com')
+        webbrowser.open(""
+                        )
         motion_detected = False
         prev_motion_time = time.time()
         os.startfile(filepath)
@@ -61,7 +59,6 @@ while True:
         break
 
     motion_counter += 1
-
 
 cap.release()
 cv2.destroyAllWindows()
