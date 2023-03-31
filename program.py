b7961e6cbfe2405b48407c235f64de565f86541f
raphael-gi <raphael.gisler@stud.edubs.ch>
2023-03-31 20:29:47 +0200

    Change to grid layout

--- program.py
+++ program.py
@@ -4,9 +4,6 @@ import os
 
 config = ConfigParser()
 
-
-
-
 def save(Delay="", RTSP="", Website="", Program=""):
     config.set("DEFAULT", "Delay", Delay)
     config.set("DEFAULT", "RTSP", RTSP)
@@ -17,6 +14,16 @@ def save(Delay="", RTSP="", Website="", Program=""):
 
     os.system("main.py")
 
+def addInputs(items):
+    for idx, item in enumerate(items):
+        item[0].grid(row=idx, column=0, pady=5)
+        item[1].grid(row=idx, column=1, pady=5)
+
+def showEmail(state):
+    if state:
+        emailFrame.grid(row=0, column=1)
+    else:
+        emailFrame.grid_remove()
 
 root = tk.Tk()
 
@@ -25,36 +32,42 @@ root.resizable(False, False)
 root.geometry("600x400")
 root.eval('tk::PlaceWindow . center')
 
-contentPanel = tk.PanedWindow()
-emailPanel = tk.PanedWindow()
+frame = tk.Frame(root)
+frame.pack()
+contentFrame = tk.LabelFrame(frame, text="Einstellungen")
+contentFrame.grid(row=0, column=0)
 
-rtpsLabel = tk.Label(text="RTPS")
-rtps = tk.Entry(root)
+emailFrame = tk.LabelFrame(frame, text="Email")
+emailFrame.grid(row=0, column=1)
 
-programLabel = tk.Label(text="Programm")
-program = tk.Entry(root)
+# Content for Content Frame
+rtpsLabel = tk.Label(contentFrame, text="RTPS")
+rtps = tk.Entry(contentFrame)
+programLabel = tk.Label(contentFrame, text="Programm")
+program = tk.Entry(contentFrame)
+websiteLabel = tk.Label(contentFrame, text="Website")
+website = tk.Entry(contentFrame)
+delayLabel = tk.Label(contentFrame, text="Delay")
+delay = tk.Entry(contentFrame)
+sendEmailLabel = tk.Label(contentFrame, text="Delay")
+sendEmail = tk.Checkbutton(contentFrame, onvalue=1, offvalue=0)
 
-websiteLabel = tk.Label(text="website")
-website = tk.Entry(root)
+# Content for Email Frame
+emailAdressLabel = tk.Label(emailFrame, text="Email Adresse")
+emailAdress = tk.Entry(emailFrame)
+emailSubjectLabel = tk.Label(emailFrame, text="Email Subject")
+emailSubject = tk.Entry(emailFrame)
+emailContentLabel = tk.Label(emailFrame, text="Email Content")
+emailContent = tk.Text(emailFrame, height=5, width=15)
 
-delayLabel = tk.Label(text="delay")
-delay = tk.Entry(root)
+# Add the items to the Frame
+contentItems = [[rtpsLabel,rtps],[programLabel,program],[websiteLabel,website],[delayLabel,delay],[sendEmailLabel, sendEmail]]
+emailItems = [[emailAdressLabel,emailAdress],[emailSubjectLabel, emailSubject],[emailContentLabel,emailContent]]
+addInputs(contentItems)
+addInputs(emailItems)
 
-emailAdressLabel = tk.Label(text="Email Adresse")
-emailAdress = tk.Entry(root)
 
-b = tk.Button(text="save&restart", command=lambda: save(delay.get(), rtps.get(), program.get(), website.get()))
+saveButton = tk.Button(text="save&restart", command=lambda: save(delay.get(), rtps.get(), program.get(), website.get()))
 
-contentPanel.add(rtpsLabel)
-contentPanel.add(rtps)
-contentPanel.add(programLabel)
-contentPanel.add(program)
-
-emailPanel.add(emailAdressLabel)
-emailPanel.add(emailAdress)
-
-contentPanel.pack(side="left")
-emailPanel.pack(side="right")
-b.pack(side="bottom")
 
 root.mainloop()
