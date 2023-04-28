1771b8e36678f3062a427e1327019e69f91e78a7
raphael-gi <raphael.gisler@stud.edubs.ch>
2023-04-29 00:04:07 +0200

    Add default values for content frame

--- program.py
+++ program.py
@@ -6,8 +6,8 @@ config = ConfigParser()
 
 
 def save(Delay="", RTSP="", Website="", Program="",
-         EmailState=False, EmailAdress="", EmailSubject="", EmailContent="",
-         MessengerState=False, MessengerNumber="", MessengerContent=""):
+         EmailState=0, EmailAdress="", EmailSubject="", EmailContent="",
+         SmsState=0, MessengerNumber="", MessengerContent=""):
     config.set("DEFAULT", "Delay", Delay)
     config.set("DEFAULT", "RTSP", RTSP)
     config.set("DEFAULT", "Program", Program)
@@ -16,7 +16,7 @@ def save(Delay="", RTSP="", Website="", Program="",
     config.set("DEFAULT", "EmailAddress", EmailAdress)
     config.set("DEFAULT", "EmailSubject", EmailSubject)
     config.set("DEFAULT", "EmailContent", EmailContent)
-    config.set("DEFAULT", "MessengerState", f"{MessengerState}")
+    config.set("DEFAULT", "smsState", f"{SmsState}")
     config.set("DEFAULT", "MessengerNumber", MessengerNumber)
     config.set("DEFAULT", "MessengerContent", MessengerContent)
     with open('settings.ini', 'w') as f:
@@ -63,17 +63,26 @@ emailFrame.grid_remove()
 smsFrame = tk.LabelFrame(frame, text="SMS")
 smsFrame.grid_remove()
 
+config.read("settings.ini")
 # Content for Content Frame
-rtpsLabel = tk.Label(contentFrame, text="RTPS")
-rtps = tk.Entry(contentFrame)
+rtspLabel = tk.Label(contentFrame, text="RTPS")
+rtsp = tk.Entry(contentFrame)
+rtsp.insert(0, config.get("DEFAULT", "rtsp"))
 programLabel = tk.Label(contentFrame, text="Program")
 program = tk.Entry(contentFrame)
+program.insert(0, config.get("DEFAULT", "program"))
 websiteLabel = tk.Label(contentFrame, text="Website")
 website = tk.Entry(contentFrame)
+website.insert(0, config.get("DEFAULT", "website"))
 delayLabel = tk.Label(contentFrame, text="Delay")
 delay = tk.Entry(contentFrame)
-emailState = tk.BooleanVar()
-smsState = tk.BooleanVar()
+delay.insert(0, config.get("DEFAULT", "delay"))
+emailState = tk.IntVar(value=int(config.get("DEFAULT", "emailState")))
+if emailState.get():
+    showEmail(True)
+smsState = tk.IntVar(value=int(config.get("DEFAULT", "smsState")))
+if smsState.get():
+    showSms(True)
 sendEmailLabel = tk.Label(contentFrame, text="Send Email")
 sendEmail = tk.Checkbutton(contentFrame, onvalue=1, offvalue=0, variable=emailState,
                            command=lambda: showEmail(emailState.get()))
@@ -103,7 +112,7 @@ smsContentLabel = tk.Label(smsFrame, text="SMS Text")
 smsContent = tk.Text(smsFrame, height=5, width=15)
 
 # Add the items to the Frame
-contentItems = [[rtpsLabel, rtps], [programLabel, program], [websiteLabel, website], [delayLabel, delay],
+contentItems = [[rtspLabel, rtsp], [programLabel, program], [websiteLabel, website], [delayLabel, delay],
                 [sendEmailLabel, sendEmail], [sendSmsLabel, sendSms]]
 emailItems = [[emailPasswordLabel, emailPassword], [emailAddressLabel, emailAddress], [emailSubjectLabel, emailSubject],
               [emailContentLabel, emailContent]]
@@ -113,7 +122,7 @@ addInputs(contentItems)
 addInputs(emailItems)
 addInputs(smsItems)
 
-saveButton = tk.Button(text="Save", command=lambda: save(delay.get(), rtps.get(), program.get(), website.get(),
+saveButton = tk.Button(text="Save", command=lambda: save(delay.get(), rtsp.get(), program.get(), website.get(),
                                                          emailState.get(), emailAddress.get(), emailSubject.get(),
                                                          emailContent.get(1.0, "end-1c"),
                                                          smsState.get(), smsNumber.get(),
